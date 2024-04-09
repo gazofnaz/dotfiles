4155f7977e146b19da5a3b9fdeda301ce95bcad4
Gareth Arnott <gareth.arnott@thirdbridge.com>
2024-04-09 14:11:19 +0100

    disable spellcheck in markdown. nicer scrolling

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,3 +2,14 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
+
+-- pandoc related
+
+vim.opt.spell = false
+vim.opt.foldmethod = "manual"
+vim.opt.foldenable = false
+
+-- scrolling
+vim.opt.relativenumber = false
+vim.opt.scrolloff = 8
+vim.opt.linebreak = true
