002a69e1de8dc07b3738658198c61d1bd3f5b1f8
Gareth Arnott <gareth.arnott@thirdbridge.com>
2024-04-12 21:17:17 +0100

    add base16 colorschemes. set gruvbox default

---
+++ nvim/lua/plugins/base16.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "RRethy/base16-nvim",
+    lazy = true,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "base16-gruvbox-material-dark-hard",
+    },
+  },
+}
