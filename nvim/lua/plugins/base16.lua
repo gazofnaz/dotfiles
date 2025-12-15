41ecc01ca0c90d25828a276cdad67d1cd7e77f39
gazofnaz <gazofnaz31@hotmail.com>
2025-12-15 15:00:17 +0000

    various changes, fixes to colours

--- nvim/lua/plugins/base16.lua
+++ nvim/lua/plugins/base16.lua
@@ -6,7 +6,11 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "base16-google-light",
+      -- colorscheme = "base16-cupertino",
+      -- colorscheme = "base16-ia-light",
+      -- colorscheme = "base16-catppuccin-latte",
+      -- colorscheme = "base16-one-light",
+      colorscheme = "base16-summerfruit-light",
     },
   },
 }
--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,6 +1,7 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    version = "v1.0.0",
     ---@class PluginLspOpts
     opts = {
       ---@type lspconfig.options
